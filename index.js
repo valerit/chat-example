b23971d8380f8e379d6538485686ada18e76a696
valeri <valeri.tsertsvadze.4@gmail.com>
2018-05-30 22:17:58 +0400

    use websocket only

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var server = require('express')();
 var serverHttp = require('http').Server(server);
 
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, { transports: ['websocket']});
 var port = process.env.PORT || 3000;
 var serverPort = 3001;
 
